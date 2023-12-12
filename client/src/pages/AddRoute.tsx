8ee7378db92fba8e5572cfc296b2d05714efb213
Ajmir Raziqi <ajmir.ng3@gmail.com>
2023-12-12 13:40:41 +0200

    imporve add post

--- client/src/pages/AddRoute.tsx
+++ client/src/pages/AddRoute.tsx
@@ -1,12 +1,14 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Textarea } from "@/components/ui/textarea";
 
 function AddTextPost() {
   return (
-    <div className="flex flex-col items-start gap-4">
+    <div className="flex flex-col gap-4 p-1">
       <Textarea
-        className="textarea textarea-bordered w-full h-auto textarea-lg"
+        className="textarea w-full h-auto textarea-lg"
         placeholder="Type here!"
       ></Textarea>
       <Button>Post</Button>
@@ -16,15 +18,13 @@ function AddTextPost() {
 
 function AddImagePost() {
   return (
-    <div className="flex flex-col items-start gap-4">
-      <input
-        type="file"
-        className="file-input file-input-bordered w-full"
-        multiple
-        accept="image/*"
-      />
+    <div className="flex flex-col gap-4 p-1">
+      <div className="grid w-full gap-2">
+        <Label htmlFor="images">Images</Label>
+        <Input id="images" type="file" multiple accept="image/*" />
+      </div>
       <Textarea
-        className="textarea textarea-bordered w-full h-auto textarea-lg"
+        className="textarea  w-full h-auto textarea-lg"
         placeholder="Desription here!"
       ></Textarea>
       <Button>Upload and Post</Button>
@@ -33,14 +33,13 @@ function AddImagePost() {
 }
 function AddVideoPost() {
   return (
-    <div className="flex flex-col items-start gap-4">
-      <input
-        type="file"
-        className="file-input file-input-bordered w-full"
-        accept="video/*"
-      />
+    <div className="flex flex-col gap-4 p-1">
+      <div className="grid w-full gap-2">
+        <Label htmlFor="video">Video</Label>
+        <Input id="video" type="file" accept="video/*" />
+      </div>
       <Textarea
-        className="textarea textarea-bordered w-full h-auto textarea-lg"
+        className="textarea  w-full h-auto textarea-lg"
         placeholder="Desription here!"
       ></Textarea>
       <Button>Upload and Post</Button>
@@ -50,23 +49,21 @@ function AddVideoPost() {
 
 export default function AddRoute() {
   return (
-    <div className="p-2 md:p-4">
-      <Tabs defaultValue="account">
-        <TabsList className="grid w-full grid-cols-3">
-          <TabsTrigger value="post-tab">Post</TabsTrigger>
-          <TabsTrigger value="image-tab">Image/s</TabsTrigger>
-          <TabsTrigger value="video-tab">Video</TabsTrigger>
-        </TabsList>
-        <TabsContent value="post-tab">
-          <AddTextPost />
-        </TabsContent>
-        <TabsContent value="image-tab">
-          <AddImagePost />
-        </TabsContent>
-        <TabsContent value="video-tab">
-          <AddVideoPost />
-        </TabsContent>
-      </Tabs>
-    </div>
+    <Tabs defaultValue="post-tab">
+      <TabsList className="grid w-full grid-cols-3">
+        <TabsTrigger value="post-tab">Post</TabsTrigger>
+        <TabsTrigger value="image-tab">Image/s</TabsTrigger>
+        <TabsTrigger value="video-tab">Video</TabsTrigger>
+      </TabsList>
+      <TabsContent value="post-tab">
+        <AddTextPost />
+      </TabsContent>
+      <TabsContent value="image-tab">
+        <AddImagePost />
+      </TabsContent>
+      <TabsContent value="video-tab">
+        <AddVideoPost />
+      </TabsContent>
+    </Tabs>
   );
 }
